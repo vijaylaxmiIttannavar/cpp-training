8a554986627ff08f58ea6d7bc4b825d762061363
vijaylaxmiIttannavar <vijaylaxmiittannavar@gmail.com>
2025-04-29 11:04:12 +0530

    updated again

--- assessements/week02/week02/problem3.cpp
+++ assessements/week02/week02/problem3.cpp
@@ -1,28 +1,33 @@
 /* problem - 3)armstrong problem */
 
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
 	int n;
 	cin >> n;
-	int temp = n;
+	int temp = 0;
+	int num = 0;
 	int d = 0, sum = 0;
 
-	while (n > 0)
+	while (temp != 0)
 	{
-		d = n % 10;
-		sum = sum + d * d * d;
-		n = n / 10;
+		temp /= 10;
+		num++
 	}
-	//cout << sum;
+	int temp = n;
 
+	while (temp != 0)
+	{
+		d = temp % 10;
+		sum = sum + pow(d, num);
+		temp = temp / 10;
+	}
+}
+if(sum == n)
 	if (sum == temp)
-	{
 		cout << "Armstrong number" << endl;
-	}
 	else
-	{
 		cout << "not armstrong" << endl;
-	}
 }
